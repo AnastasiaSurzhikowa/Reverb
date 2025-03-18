b3f8101e31a6ad7aadb3d065aeac1422db3d2237
Coffein0 <danuha1005@mail.ru>
2025-03-18 05:13:14 +0300

    add login and logout

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <ModeToggle />
+    </div>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,11 @@
 import type { Metadata } from "next";
-import { Open_Sans, Geist, Geist_Mono } from "next/font/google";
+import { Open_Sans} from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const font = Open_Sans({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const fontMono = Open_Sans({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -23,12 +20,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${font.variable} ${fontMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={font.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
